a5d4b2320f4c3e633c7f2cbbacecfe5b554b01f9
Paolo Bonzini <pbonzini@redhat.com>
2012-02-21 14:34:53 +0100

    qemu-timer: clean up qemu_next_alarm_deadline
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- qemu-timer.c
+++ qemu-timer.c
@@ -107,27 +107,20 @@ static inline int alarm_has_dynticks(struct qemu_alarm_timer *t)
 static int64_t qemu_next_alarm_deadline(void)
 {
     int64_t delta;
-    int64_t rtdelta;
+    int64_t hdelta, rtdelta;
 
-    if (!use_icount && vm_clock->active_timers) {
-        delta = vm_clock->active_timers->expire_time -
-                     qemu_get_clock_ns(vm_clock);
+    if (!use_icount) {
+        delta = qemu_clock_deadline(vm_clock);
     } else {
         delta = INT32_MAX;
     }
-    if (host_clock->active_timers) {
-        int64_t hdelta = host_clock->active_timers->expire_time -
-                 qemu_get_clock_ns(host_clock);
-        if (hdelta < delta) {
-            delta = hdelta;
-        }
+    hdelta = qemu_clock_deadline(host_clock);
+    if (hdelta < delta) {
+        delta = hdelta;
     }
-    if (rt_clock->active_timers) {
-        rtdelta = (rt_clock->active_timers->expire_time -
-                 qemu_get_clock_ns(rt_clock));
-        if (rtdelta < delta) {
-            delta = rtdelta;
-        }
+    rtdelta = qemu_clock_deadline(rt_clock);
+    if (rtdelta < delta) {
+        delta = rtdelta;
     }
 
     return delta;
