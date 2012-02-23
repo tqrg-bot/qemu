bf6ae36b982cd968cb0539ddd80e6a721f0d07fc
Paolo Bonzini <pbonzini@redhat.com>
2012-02-23 16:47:33 +0100

    pass bottom halves to aio.c
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- aio.c
+++ aio.c
@@ -105,21 +105,26 @@ void qemu_aio_flush(void)
     AioHandler *node;
     int ret;
 
-    do {
+    /*
+     * If there are pending emulated aio start them now so flush
+     * will be able to return 1.
+     */
+    qemu_bh_poll();
+
+    for (;;) {
         ret = 0;
-
-	/*
-	 * If there are pending emulated aio start them now so flush
-	 * will be able to return 1.
-	 */
-        qemu_aio_wait();
-
         QLIST_FOREACH(node, &aio_handlers, node) {
             if (node->io_flush) {
                 ret |= node->io_flush(node->opaque);
             }
         }
-    } while (qemu_bh_poll() || ret > 0);
+
+        if (ret == 0) {
+            break;
+        }
+
+        qemu_aio_wait();
+    }
 }
 
 int qemu_aio_process_queue(void)
