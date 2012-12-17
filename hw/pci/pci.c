e4f6753373d21e3c97201af8152785d2d95ccb02
Paolo Bonzini <pbonzini@redhat.com>
2012-12-17 10:39:59 +0100

    pci: clean up resetting of IRQs
    A PCI bus reset will deassert the INTX pins, and this will make the
    irq_count array all-zeroes.  Check that this is the case, and remove
    the existing loop which might even unsync irq_count and irq_state.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- hw/pci/pci.c
+++ hw/pci/pci.c
@@ -213,13 +213,13 @@ void pci_bus_reset(PCIBus *bus)
 {
     int i;
 
-    for (i = 0; i < bus->nirq; i++) {
-        bus->irq_count[i] = 0;
-    }
     for (i = 0; i < ARRAY_SIZE(bus->devices); ++i) {
         if (bus->devices[i]) {
             pci_device_reset(bus->devices[i]);
         }
+    }
+    for (i = 0; i < bus->nirq; i++) {
+        assert(bus->irq_count[i] == 0);
     }
 }
 
