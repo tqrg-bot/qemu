2a4ec26f6daa95dcd3c9771d3a20535b0b927e3d
Paolo Bonzini <pbonzini@redhat.com>
2012-12-17 10:50:16 +0100

    qdev: switch reset to post-order
    Post-order is the only sensible direction for the reset signals.  For example,
    suppose pre-order is used and the parent has some data structures that cache
    children state (for example a list of active requests).  When the reset
    method is invoked on the parent, these caches could be in any state.
    
    If post-order is used, on the other hand, these will be in a known state
    when the reset method is invoked on the parent.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- hw/qdev.c
+++ hw/qdev.c
@@ -227,12 +227,12 @@ static int qbus_reset_one(BusState *bus, void *opaque)
 
 void qdev_reset_all(DeviceState *dev)
 {
-    qdev_walk_children(dev, qdev_reset_one, qbus_reset_one, NULL, NULL, NULL);
+    qdev_walk_children(dev, NULL, NULL, qdev_reset_one, qbus_reset_one, NULL);
 }
 
 void qbus_reset_all(BusState *bus)
 {
-    qbus_walk_children(bus, qdev_reset_one, qbus_reset_one, NULL, NULL, NULL);
+    qbus_walk_children(bus, NULL, NULL, qdev_reset_one, qbus_reset_one, NULL);
 }
 
 void qbus_reset_all_fn(void *opaque)
