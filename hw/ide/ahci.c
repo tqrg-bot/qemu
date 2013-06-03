640cefeb25e6af6c7e7a26a1b819ee1e36215ada
Paolo Bonzini <pbonzini@redhat.com>
2013-06-03 17:41:00 +0200

    ahci: reclaim memory in instance_finalize instead of exit
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- hw/ide/ahci.c
+++ hw/ide/ahci.c
@@ -1185,7 +1185,7 @@ void ahci_init(AHCIState *s, DeviceState *qdev, AddressSpace *as, int ports)
     }
 }
 
-void ahci_uninit(AHCIState *s)
+void ahci_instance_finalize(AHCIState *s)
 {
     memory_region_destroy(&s->mem);
     memory_region_destroy(&s->idp);
