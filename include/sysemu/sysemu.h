9eb4372e8db6f7f4d0c4fafffcee0b3da437185d
Wanlong Gao <gaowanlong@cn.fujitsu.com>
2013-12-04 15:58:55 +0800

    NUMA: expand MAX_NODES from 64 to 128
    libnuma choosed 128 for MAX_NODES, so we follow libnuma here.
    
    Signed-off-by: Wanlong Gao <gaowanlong@cn.fujitsu.com>
    Reviewed-by: Eduardo Habkost <ehabkost@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- include/sysemu/sysemu.h
+++ include/sysemu/sysemu.h
@@ -133,7 +133,7 @@ extern size_t boot_splash_filedata_size;
 extern uint8_t qemu_extra_params_fw[2];
 extern QEMUClockType rtc_clock;
 
-#define MAX_NODES 64
+#define MAX_NODES 128
 #define MAX_CPUMASK_BITS 255
 extern int nb_numa_nodes;
 typedef struct node_info {
