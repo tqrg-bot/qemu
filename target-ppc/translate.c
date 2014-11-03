b28a0a6ea560d9a5489a69a61944efb8b6745724
Tom Musta <tommusta@gmail.com>
2014-11-03 14:01:17 -0600

    target-ppc: Eliminate set_fprf Argument From helper_compute_fprf
    The set_fprf argument to the helper_compute_fprf helper function
    is no longer necessary -- the helper is only invoked when FPSCR[FPRF]
    is going to be set.
    
    Eliminate the unnecessary argument from the function signature and
    its corresponding implementation.  Change the return value of the
    helper to "void".  Update the name of the local variable "ret" to
    "fprf", which now makes more sense.
    
    Signed-off-by: Tom Musta <tommusta@gmail.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- target-ppc/translate.c
+++ target-ppc/translate.c
@@ -267,14 +267,8 @@ static void gen_set_cr1_from_fpscr(DisasContext *ctx)
 
 static inline void gen_compute_fprf(TCGv_i64 arg)
 {
-    TCGv_i32 t0 = tcg_temp_new_i32();
-
-    /* This case might be optimized later */
-    tcg_gen_movi_i32(t0, 1);
-    gen_helper_compute_fprf(t0, cpu_env, arg, t0);
+    gen_helper_compute_fprf(cpu_env, arg);
     gen_helper_float_check_status(cpu_env);
-
-    tcg_temp_free_i32(t0);
 }
 
 static inline void gen_set_access_type(DisasContext *ctx, int access_type)
