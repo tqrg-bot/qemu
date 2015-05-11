caf4f7700205a7a3da741fa035972d9e8e9736d5
Paolo Bonzini <pbonzini@redhat.com>
2015-05-11 17:51:05 +0200

    async: optimize aio_bh_poll
    Avoid entering the slow path of qemu_lockcnt_dec_and_lock if
    no bottom half has to be deleted.  If a bottom half deletes itself,
    it will be picked up on the next visit of the list, or when the
    AioContext itself is finalized.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- async.c
+++ async.c
@@ -70,19 +70,24 @@ int aio_bh_poll(AioContext *ctx)
 {
     QEMUBH *bh, **bhp, *next;
     int ret;
+    bool deleted = false;
 
     qemu_lockcnt_inc(&ctx->list_lock);
 
     ret = 0;
     for (bh = atomic_rcu_read(&ctx->first_bh); bh; bh = next) {
         next = atomic_rcu_read(&bh->next);
+        if (bh->deleted) {
+            deleted = true;
+            continue;
+        }
         /* The atomic_xchg is paired with the one in qemu_bh_schedule.  The
          * implicit memory barrier ensures that the callback sees all writes
          * done by the scheduling thread.  It also ensures that the scheduling
          * thread sees the zero before bh->cb has run, and thus will call
          * aio_notify again if necessary.
          */
-        if (!bh->deleted && atomic_xchg(&bh->scheduled, 0)) {
+        if (atomic_xchg(&bh->scheduled, 0)) {
             /* Idle BHs don't count as progress */
             if (!bh->idle) {
                 ret = 1;
@@ -93,6 +98,11 @@ int aio_bh_poll(AioContext *ctx)
     }
 
     /* remove deleted bhs */
+    if (!deleted) {
+        qemu_lockcnt_dec(&ctx->list_lock);
+        return ret;
+    }
+
     if (qemu_lockcnt_dec_and_lock(&ctx->list_lock)) {
         bhp = &ctx->first_bh;
         while (*bhp) {
@@ -106,7 +116,6 @@ int aio_bh_poll(AioContext *ctx)
         }
         qemu_lockcnt_unlock(&ctx->list_lock);
     }
-
     return ret;
 }
 
