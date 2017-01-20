3e3006463d10e077b468138e3a3ab21b291bf3d6
Phil Dennis-Jordan <phil@philjordan.eu>
2017-01-20 15:11:36 +0100

    pc: Enable vmware-cpuid-freq CPU option for 2.9+ machine types
    Signed-off-by: Phil Dennis-Jordan <phil@philjordan.eu>
    Message-Id: <1484921496-11257-4-git-send-email-phil@philjordan.eu>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- include/hw/i386/pc.h
+++ include/hw/i386/pc.h
@@ -381,6 +381,11 @@ bool e820_get_entry(int, uint32_t, uint64_t *, uint64_t *);
         .property = "x-mach-use-reliable-get-clock",\
         .value    = "off",\
     },\
+    {\
+        .driver   = TYPE_X86_CPU,\
+        .property = "vmware-cpuid-freq",\
+        .value    = "off",\
+    },
 
 #define PC_COMPAT_2_7 \
     HW_COMPAT_2_7 \
