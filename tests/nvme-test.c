69440fce43eee5efff5017b0e4a1d25b10204522
Emanuele Giuseppe Esposito <e.emanuelegiuseppe@gmail.com>
2018-08-17 13:16:07 +0200

    test/qgraph: nvme test node
    Convert tests/nvme-test to a driver node; currently it runs
    the PCI nop test only.
    
    Signed-off-by: Emanuele Giuseppe Esposito <e.emanuelegiuseppe@gmail.com>

--- tests/nvme-test.c
+++ tests/nvme-test.c
@@ -9,24 +9,57 @@
 
 #include "qemu/osdep.h"
 #include "libqtest.h"
+#include "libqos/qgraph.h"
+#include "libqos/pci.h"
 
-/* Tests only initialization so far. TODO: Replace with functional tests */
-static void nop(void)
+typedef struct QNvme QNvme;
+
+struct QNvme {
+    QOSGraphObject obj;
+    QPCIDevice dev;
+};
+
+static void nvme_destructor(QOSGraphObject *obj)
 {
+    QNvme *nvme = (QNvme *)obj;
+    g_free(nvme);
 }
 
-int main(int argc, char **argv)
+static void *nvme_get_driver(void *obj, const char *interface)
 {
-    int ret;
+    QNvme *nvme = obj;
 
-    g_test_init(&argc, &argv, NULL);
-    qtest_add_func("/nvme/nop", nop);
+    if (!g_strcmp0(interface, "pci-device")) {
+        return &nvme->dev;
+    }
 
-    qtest_start("-drive id=drv0,if=none,file=null-co://,format=raw "
-                "-device nvme,drive=drv0,serial=foo");
-    ret = g_test_run();
-
-    qtest_end();
-
-    return ret;
+    fprintf(stderr, "%s not present in nvme\n", interface);
+    g_assert_not_reached();
 }
+
+static void *nvme_create(void *pci_bus, QGuestAllocator *alloc, void *addr)
+{
+    QNvme *nvme = g_new0(QNvme, 1);
+    QPCIBus *bus = pci_bus;
+
+    qpci_device_init(&nvme->dev, bus, addr);
+    nvme->obj.get_driver = nvme_get_driver;
+    nvme->obj.destructor = nvme_destructor;
+
+    return &nvme->obj;
+}
+
+static void nvme_register_nodes(void)
+{
+    QOSGraphEdgeOptions opts = {
+        .extra_device_opts = "addr=04.0,drive=drv0,serial=foo",
+        .before_cmd_line = "-drive id=drv0,if=none,file=null-co://,format=raw",
+    };
+    add_qpci_address(&opts, &(QPCIAddress) { .devfn = QPCI_DEVFN(4, 0) });
+
+    qos_node_create_driver("nvme", nvme_create);
+    qos_node_consumes("nvme", "pci-bus", &opts);
+    qos_node_produces("nvme", "pci-device");
+}
+
+libqos_init(nvme_register_nodes);
